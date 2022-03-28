ea014a400b32498637f2761798632f3dcd4c9df2
Jack Fung <fungsaulok524@gmail.com>
2022-03-29 03:01:14 +0800

    inital upload

---
+++ hkdfbkdsf.py
@@ -0,0 +1,132 @@
+from asyncore import write
+from imghdr import what
+import paramiko
+import os
+import requests as rq
+import json
+import sys
+tries = 1
+temp1 = ' '
+manifestask = ' '
+manifestfile = ' '
+jsonreal = 'false'
+filenotfoundmsg = False
+##much cool functions
+def askfile():
+    global tries
+    global manifestask
+    if tries > 10:
+        print('\033[91m=>[FATAL] time was wasted.')
+        exit()
+    manifestask = input(f'\033[91m=>where the fuck is the file? [{tries}/10] (type exit to leave)\033[90m\n==> ')
+    if manifestask == 'exit':
+        exit()
+    tries = tries + 1
+def jsoncheck(file):
+    global jsonreal
+    with file as f:
+        try:
+            return json.load(f) # put JSON-data to a variable
+        except json.decoder.JSONDecodeError:
+            jsonreal = 'false'
+        else:
+            jsonreal = 'true'
+
+currentdir = os.getcwd()
+
+##make sure the loop isnt exitable unless manifest.json is found
+while True:
+    ##try finding file in current directory
+    try:
+        manifestfile = open(currentdir + '/manifest.json')
+        manifestfile = currentdir + '/manifest.json'
+        print('\033[92m=>[SUCCESS] manifest.json found!')
+        break
+    except:FileNotFoundError
+    if True == False:
+        ifuckedup = True
+    else:
+        if filenotfoundmsg == False:
+            print('\033[93m=>[WARNING] manifest.json not found in current folder.')
+            filenotfoundmsg = True
+        manifestfile = askfile()
+        ##check if file exists
+        if not os.path.isfile(manifestask):
+            print("\033[93m=>[WARNING] file doesn't fucking exist. try again.")
+            askfile()
+        else:
+            ##check if file is a json formatted file
+            jsoncheck(manifestask)
+            if jsonreal == 'true':
+                print('\033[92m=>[SUCCESS] manifest.json found!')
+                ##leave loop
+                manifestfile = manifestask
+                break
+            elif jsonreal == 'false':
+                print('\033[91m=>[FATAL] manifest.json is not a json formatted file. try again.')
+                askfile()
+            else:
+                print('\033[91m=>[FATAL] something went very wrong. try again.')
+                exit()
+
+manifest = ' '
+
+##open file but make sure the file is there
+
+with open(manifestfile) as f:
+    manifest = json.load(f)
+    print('\033[97m ')
+FILE_IDS = [file["fileID"] for file in manifest["files"]]
+PROJECT_IDS = [file["projectID"] for file in manifest["files"]] # Project IDs of the mods, as shown on the mod page
+VERSION = manifest["minecraft"]["version"] # Name of the game version
+
+for i, id in enumerate(PROJECT_IDS):
+    ticker = f"[{i+1}/{len(PROJECT_IDS)}]" # For showing which mod we're on
+
+    # Get mod name, so it looks nice
+    data = json.loads(rq.get(f"https://curse.nikky.moe/api/addon/{id}").content) # Put the request's data into a Python-readable format
+    modName = data["name"]
+
+    # Get mod's latest version for this game version
+    print(f"{ticker} Getting mod download link for mod {modName} (ID: {id})...")
+    data = json.loads(rq.get(f"https://curse.nikky.moe/api/addon/{id}/files").content)
+    # Get the correct version matching the file ID for this mod
+    correctVersion = {}
+    try:
+        correctVersion = [candidate for candidate in data if FILE_IDS[i] == candidate["id"]][0]
+        print(f"=>{ticker} Found correct mod version (File ID: {FILE_IDS[i]} for mod {modName} (ID: {id})!")
+    except:
+        print(f"=>{ticker} Couldn't find a matching file ID ({FILE_IDS[i]}) for mod {modName} (ID: {id}). Downloading latest version for the current forge version...")
+        correctVersion = [candidate for candidate in data if VERSION == candidate["gameVersion"][0]]
+    try:
+            print(f"=>{ticker} Starting download of mod {modName} (ID: {id})...")
+            ##write to output.txt
+            #create output.txt if not exist
+            if not os.path.isfile("output.txt"):
+                open("output.txt", "w").close()
+            with open("output.txt", "a") as f:
+                f.write(correctVersion["downloadUrl"] + "\n")
+                ##close file because why not
+                f.close()
+                print(f"=>{ticker} added mod {modName} (ID: {id}) to output.txt!")
+    except: # The fileID isn't available
+        print(f"=>{ticker} {modName} failed processing! (ID: {id})")
+        sys.exit()
+
+print(f"=>Finished proc all {len(PROJECT_IDS)} mods!")
+print("\033[97m=>uploading to server...")
+
+host = "192.168.99.45"
+port = 22
+transport = paramiko.Transport((host, port))
+usr = "pi"
+passwd = "raspberry"
+transport.connect(username = usr, password = passwd)
+sftp = paramiko.SFTPClient.from_transport(transport)
+
+path = currentdir + "/output.txt"
+sftp.put(path, "/home/pi/modlist/output.txt")
+
+sftp.close()
+transport.close()
+print("\033[97m=>done.")
---
+++ moddownloader.bat
@@ -0,0 +1,30 @@
+echo 'welcom to jack mod install' 
+echo 'the script now uses python.'
+echo 'please wait while we install python + needed libraries.'
+python --version >NUL
+if errorlevel 0 goto python2_installed
+echo 'python 2 not found. trying to find python 3'
+python3 --version >NUL
+if errorlevel 0 goto python3_installed
+echo 'python not installed. installing..'
+powershell cd \jacktempfolder\py; start-Bitstransfer https://www.python.org/ftp/python/3.10.4/python-3.10.4-amd64.exe
+cd \jacktempfolder\py
+python-3.10.4-amd64.exe /quiet InstallAllUsers=1 PrependPath=1
+:python2_installed
+echo 'needed python3. installing..'
+powershell cd \jacktempfolder\py; start-Bitstransfer https://www.python.org/ftp/python/3.10.4/python-3.10.4-amd64.exe
+cd \jacktempfolder\py
+python-3.10.4-amd64.exe /quiet InstallAllUsers=1 PrependPath=1
+python3 --version >NUL
+if errorlevel 0 goto python3_installed
+:python3_installed
+echo 'python3 installed.'
+pip3 install paramiko
+cd \jacktempfolder
+::check if python script exists
+if exist moddownloader.py (
+echo 'python script found. skipping download.'
+else
+echo 'python script not found.'
+echo 'downloading python script..'
+powershell cd \jacktempfolder; start-Bitstransfer 
