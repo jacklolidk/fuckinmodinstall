5464f624b2df0ba9b23a182ffc153f3e03b25840
Jack Fung <fungsaulok524@gmail.com>
2022-03-29 11:45:07 +0800

    add correct paths and make folders

--- moddownloader.bat
+++ moddownloader.bat
@@ -8,31 +8,33 @@ echo 'python 2 not found. trying to find python 3'
 python3 --version >NUL
 if errorlevel 0 goto python3_installed
 echo 'python not installed. installing..'
-powershell cd \jacktempfolder\py; start-Bitstransfer https://www.python.org/ftp/python/3.10.4/python-3.10.4-amd64.exe
-cd \jacktempfolder\py
+mkdir C:\jacktempfolder
+mkdir C:\jacktempfolder\py
+powershell cd C:\jacktempfolder\py; start-Bitstransfer https://www.python.org/ftp/python/3.10.4/python-3.10.4-amd64.exe
+cd C:\jacktempfolder\py
 python-3.10.4-amd64.exe /quiet InstallAllUsers=1 PrependPath=1
 :python2_installed
 echo 'needed python3. installing..'
-powershell cd \jacktempfolder\py; start-Bitstransfer https://www.python.org/ftp/python/3.10.4/python-3.10.4-amd64.exe
-cd \jacktempfolder\py
+powershell cd C:\jacktempfolder\py; start-Bitstransfer https://www.python.org/ftp/python/3.10.4/python-3.10.4-amd64.exe
+cd C:\jacktempfolder\py
 python-3.10.4-amd64.exe /quiet InstallAllUsers=1 PrependPath=1
 python3 --version >NUL
 if errorlevel 0 goto python3_installed
 :python3_installed
 echo 'python3 installed.'
 pip3 install paramiko
-cd \jacktempfolder
+cd C:\jacktempfolder
 ::check if python script exists
 if exist modinstall.py (
 echo 'python script found. skipping download.'
 else
 echo 'python script not found.'
 echo 'downloading python script..'
-powershell cd \jacktempfolder; start-Bitstransfer https://raw.githubusercontent.com/jacklolidk/fuckinmodinstall/modinstall.py
+powershell cd C:\jacktempfolder; start-Bitstransfer https://raw.githubusercontent.com/jacklolidk/fuckinmodinstall/modinstall.py
 fi
 echo 'should be done.'
 echo 'deleting temp folder. press any key'
 pause
 :: delete temp folder
-powershell rm -r \jacktempfolder
+powershell rm -r C:\jacktempfolder
 exit
