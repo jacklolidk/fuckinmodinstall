ec5ef42ffcb9cd881abccd0f4cf898dbe9c05fe2
jacklolidk <50368061+jacklolidk@users.noreply.github.com>
2022-03-29 12:39:35 +0800

    try 2

--- moddownloader.bat
+++ moddownloader.bat
@@ -9,11 +9,11 @@ python3 --version >NUL
 if errorlevel 0 goto python3_installed
 echo 'python not installed. installing..'
 powershell cd ~; start-Bitstransfer https://www.python.org/ftp/python/3.10.4/python-3.10.4-amd64.exe
-cd %HOMEPATH
+cd %HOMEPATH%
 python-3.10.4-amd64.exe /quiet InstallAllUsers=1 PrependPath=1
 :python2_installed
 echo 'needed python3. installing..'
-powershell cd \; start-Bitstransfer https://www.python.org/ftp/python/3.10.4/python-3.10.4-amd64.exe
+powershell cd ~; start-Bitstransfer https://www.python.org/ftp/python/3.10.4/python-3.10.4-amd64.exe
 python-3.10.4-amd64.exe /quiet InstallAllUsers=1 PrependPath=1
 python3 --version >NUL
 if errorlevel 0 goto python3_installed else echo something died
@@ -27,7 +27,7 @@ echo 'python script found. skipping download.'
 else
 echo 'python script not found.'
 echo 'downloading python script..'
-powershell cd \; start-Bitstransfer https://raw.githubusercontent.com/jacklolidk/fuckinmodinstall/modinstall.py
+powershell cd ~; start-Bitstransfer https://raw.githubusercontent.com/jacklolidk/fuckinmodinstall/modinstall.py
 fi
 echo 'should be done.'
 echo 'deleting temp folder. press any key'
